cdbef92bbad4edbce45b430b32a044e547438f7e
mandeeplambton <39274135+mandeeplambton@users.noreply.github.com>
2019-01-17 17:07:46 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c0730499
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a collection that is A list
+            var dogs = new List<string>();
+            dogs.Add("bichon");
+            dogs.Add("collia");
+            dogs.Add("retriver");
+
+            foreach (var dog in dogs)
+            {
+                Console.WriteLine(dog + " ");
+
+            }
+            int[] a1 = new int[] { 1, 3, 5, 7, 9, 11 };
+            foreach(int i in a1)
+            {
+                Console.WriteLine("the value of i");
+            
+
+        }
+    }
+}
